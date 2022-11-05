284b9e9325a07a60a558c9cc8428528ab1bdcbe6
Amir <asp20@st-andrews.ac.uk>
2022-11-05 21:26:03 +0000

    first commit

---
+++ gym-occupancy.py
@@ -0,0 +1,33 @@
+import subprocess
+import re
+import time
+from datetime import datetime
+import pytz
+import smtplib, ssl
+import sys
+import certifi
+
+port = 465  # For SSL
+context = ssl.create_default_context(cafile=certifi.where())
+email_sender = sys.argv[1]
+password_sender = sys.argv[2]
+emai_receiver = sys.argv[3]
+
+tz_GB = pytz.timezone('Europe/London')
+datetime_GB = datetime.now(tz_GB)
+
+if __name__ == '__main__':
+    while True:
+        if 22 >= datetime_GB.hour >= 7:
+            occupancyHtml = subprocess.run('curl -s https://sport.wp.st-andrews.ac.uk | grep "Occupancy"', shell=True,
+                                           text=True, capture_output=True)
+            occupancyHtmlString = occupancyHtml.stdout.upper()
+            occupancy = re.search("\\d+(?:\\.\\d+)?%", occupancyHtmlString)
+            occupancy = occupancy.group()[0:len(occupancy.group()) - 1]
+            if int(occupancy) <= 50:
+                with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+                    server.login(email_sender, password_sender)
+                    server.sendmail(email_sender, emai_receiver, "gym is at " + occupancy + "% occupancy, go gym fam.")
+
+        time.sleep(20 * 60)
+        datetime_GB = datetime.now(tz_GB)
